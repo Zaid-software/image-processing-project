83d699d50d4a3827d4088b2323ecae79921ec06e
Zaid-software <shaikabulzaid@outlook.com>
2025-05-23 11:34:43 +0700

    fix

--- retouching/js/retouch.js
+++ retouching/js/retouch.js
@@ -3,6 +3,7 @@ const canvas = document.getElementById('retouchCanvas');
 const ctx = canvas.getContext('2d');
 
 let drawing = false;
+let currentEffect = 'blur';
 
 document.getElementById('uploadRetouch').addEventListener('change', function (e) {
   const file = e.target.files[0];
@@ -14,6 +15,11 @@ document.getElementById('uploadRetouch').addEventListener('change', function (e)
   };
   reader.readAsDataURL(file);
 });
+
+
+document.getElementById('effectBlur').addEventListener('click', () => currentEffect = 'blur');
+document.getElementById('effectSepia').addEventListener('click', () => currentEffect = 'sepia');
+document.getElementById('effectRedTint').addEventListener('click', () => currentEffect = 'redtint');
 
 retouchImage.onload = function () {
   canvas.width = retouchImage.width;
@@ -32,7 +38,14 @@ canvas.addEventListener('mousemove', function (e) {
   const x = e.clientX - rect.left;
   const y = e.clientY - rect.top;
 
-  blurArea(x, y);
+
+  if (currentEffect === 'blur') {
+    blurArea(x, y);
+  } else if (currentEffect === 'sepia') {
+    applySepia(x, y);
+  } else if (currentEffect === 'redtint') {
+    applyRedTint(x, y);
+  }
 });
 
 function blurArea(x, y) {
@@ -48,12 +61,42 @@ function blurArea(x, y) {
   ctx.putImageData(imageData, x - size/2, y - size/2);
 }
 
+function applySepia(x, y) {
+  const size = 20;
+  const imageData = ctx.getImageData(x - size/2, y - size/2, size, size);
+  const data = imageData.data;
+
+  for (let i = 0; i < data.length; i += 4) {
+    const r = data[i];
+    const g = data[i + 1];
+    const b = data[i + 2];
+    
+    data[i] = Math.min(255, (r * 0.393) + (g * 0.769) + (b * 0.189));
+    data[i + 1] = Math.min(255, (r * 0.349) + (g * 0.686) + (b * 0.168));
+    data[i + 2] = Math.min(255, (r * 0.272) + (g * 0.534) + (b * 0.131));
+  }
+
+  ctx.putImageData(imageData, x - size/2, y - size/2);
+}
+
+function applyRedTint(x, y) {
+  const size = 20;
+  const imageData = ctx.getImageData(x - size/2, y - size/2, size, size);
+  const data = imageData.data;
+
+  for (let i = 0; i < data.length; i += 4) {
+    
+    data[i] = Math.min(255, data[i] * 1.5);
+    data[i + 1] = data[i + 1] * 0.8;
+    data[i + 2] = data[i + 2] * 0.8;
+  }
+
+  ctx.putImageData(imageData, x - size/2, y - size/2);
+}
+
 function downloadRetouchedImage() {
   const link = document.createElement('a');
   link.download = 'retouched-image.png';
   link.href = canvas.toDataURL('image/png');
   link.click();
 }
-
-
-
